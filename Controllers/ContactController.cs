a9b0435af574c2f1f97550afd744843ebb10f2a5
Ali Aras <aliarascc@gmail.com>
2015-02-09 20:08:47 +0200

    .net RESTful Web API

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,54 @@
+﻿using ContactManager.Models;
+using ContactManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        #region ilk yazılan kod
+        //public Contact[] Get()
+        //{
+        //    return new Contact[]{
+        //       new Contact
+        //       {
+        //           Id=1,
+        //           Name="Gleen Block"
+        //       },
+        //       new Contact
+        //       {
+        //           Id=2,
+        //           Name="Dan Roth"
+        //       }
+        //    };
+
+        //}
+        #endregion
+
+        private ContactRepository contactRepository;
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+
+        }
+
+        public Contact[] Get() 
+        {
+            return contactRepository.GetAllContacts();
+        }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
+    }
+}
